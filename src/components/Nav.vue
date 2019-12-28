da47f7ab5f36578d5f9835a958f9326b054a478f
zigelzi <miika.a.savela@gmail.com>
2019-12-28 17:09:37 +0200

    initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,5 @@
+<template>
+  <nav class="nav">
+    <router-link to="home" class="nav-link">Home</router-link>
+  </nav>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home">
+    <Nav />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Nav
   }
 };
 </script>
