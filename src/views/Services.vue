3a38d53ff3ee3b5d38395f4b82d947a0dc5e1995
zigelzi <miika.a.savela@gmail.com>
2020-02-14 20:28:01 +0200

    Removed unneccessary Tabs component

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -2,41 +2,10 @@
   <div>
     <h1 class="content-title">Huoltotiedot</h1>
     <p>Tähän tulee huoltoon liittyvät asiat</p>
-    <Tabs :tabs="tabs" />
   </div>
 </template>
 <script>
-import Tabs from "@/components/general/Tabs";
 export default {
-  components: {
-    Tabs
-  },
-  data() {
-    return {
-      activeItem: "createCategory",
-      tabs: [
-        {
-          title: "Luo kategorioita",
-          activeName: "createCategory",
-          content: "Luo kategoria sisältö",
-          link: "#luo_kategorioita"
-        },
-        {
-          title: "Luo remontti",
-          activeName: "createService",
-          content: "Luo remontti sisältö",
-          link: "#luo_remontti"
-        }
-      ]
-    };
-  },
-  methods: {
-    isActive(menuItem) {
-      return this.activeItem === menuItem;
-    },
-    setActive(menuItem) {
-      this.activeItem = menuItem;
-    }
-  }
+  components: {}
 };
 </script>
