86c3710f485e59db2cb2f8d01ab85dc5027bba26
zigelzi <miika.a.savela@gmail.com>
2020-02-18 18:39:47 +0200

    Add SelectField component

---
+++ src/components/inputs/SelectField.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form-group">
+    <label :for="inputName"><slot></slot></label>
+    <select
+      ref="selectField"
+      :id="inputName"
+      class="form-control"
+      @input="inputChanged"
+    >
+      <option value="">Ladataan...</option>
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+        >{{ option.label }}</option
+      >
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    inputName: String,
+    options: Array
+  },
+  methods: {
+    inputChanged() {
+      this.$emit("input", this.$refs.selectField.value);
+    }
+  }
+};
+</script>
--- src/views/service/CreateRepair.vue
+++ src/views/service/CreateRepair.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
     Luo korjaus
+    <div>
+      <SelectField
+        inputName="repairCategories"
+        v-model="test"
+        :options="categories"
+        >Valitse kategoria</SelectField
+      >
+    </div>
+    {{ test }}
   </div>
 </template>
+<script>
+import SelectField from "@/components/inputs/SelectField";
+export default {
+  components: {
+    SelectField
+  },
+  data() {
+    return {
+      categories: [
+        {
+          value: "category1",
+          label: "Category 1"
+        },
+        {
+          value: "category2",
+          label: "Category 2"
+        }
+      ],
+      test: ""
+    };
+  }
+};
+</script>
